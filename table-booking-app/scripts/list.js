867e4999b31deced012e9719ec64c887a4dc576b
TzeYan Low <lowt05@yahoo.com>
2020-10-23 21:28:06 +0800

    added js code 2127

---
+++ table-booking-app/scripts/list.js
@@ -0,0 +1,68 @@
+document.addEventListener("load",function(){
+    GetBookings();
+});
+  
+  function GetBookings(){
+  let url = 'https://api.sheety.co/d2f6c22aad720df1ddb808edebcc390a/bookingApIdemo/sheet1';
+  fetch(url)
+  .then((response) => response.json())
+  .then(json => {
+    // Do something with the data
+    //console.log(json.sheet1);
+    var bookings = document.getElementById("booking-list");
+    var bookingIds=[];
+
+    for (var i = 0; i < json.sheet1.length; i++){
+      var gName = json.sheet1[i].name;
+      var gEmail = json.sheet1[i].email;
+      var gPax = json.sheet1[i].pax;
+      var gId = json.sheet1[i].id;
+      var buttonId = "delete" + gId;
+
+      let row = bookings.insertRow(bookings.rows.length);
+      row.insertCell(0).innerHTML = gId;  
+      row.insertCell(1).innerHTML = gName;
+      row.insertCell(2).innerHTML = gEmail;
+      row.insertCell(3).innerHTML = gPax;
+      row.insertCell(4).innerHTML = ""; //Remarks
+      row.insertCell(5).innerHTML = "<button id='" + buttonId + "'>Delete </button></br>"; 
+
+      // bookings.innerHTML += gId + "- " + gName + ", " + gEmail + "- " + gPax;
+      // bookings.innerHTML += "<button id='" + buttonId + "'>Delete </button></br>";
+
+      bookingIds.push(buttonId);
+      
+    }
+    for (let j=0; j<bookingIds.length; j++){
+       // console.log(bookingIds[j]);
+      let el=document.getElementById(bookingIds[j]);
+      el.addEventListener("click", function(){
+       let theId=bookingIds[j].replace("delete",""); 
+       // DeleteBooking(theId);
+      });
+    }
+    
+   });
+  }
+  
+  document.getElementById("getBooking").addEventListener("click",function(){
+    GetBookings();
+  })
+  
+  
+  function DeleteBooking(id){
+    let url = 'https://api.sheety.co/d2f6c22aad720df1ddb808edebcc390a/bookingApi/sheet1/'+id;
+  fetch(url, {
+    method: 'DELETE',
+  })
+  .then((response) => {
+    document.getElementById("deleteMsg").innerHTML="Booking deleted!"
+    document.getElementById("deleteId").value="";
+    GetBookings();
+  });
+  }
+  
+  document.getElementById("deleteBooking").addEventListener("click",function(){
+      DeleteBooking(document.getElementById("deleteId").value);
+    });
+    
