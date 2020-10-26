27048aa758ee41290c39f171a6840dae53ae72dc
TzeYan Low <lowt05@yahoo.com>
2020-10-26 23:16:36 +0800

    2316

--- table-booking-app/scripts/list.js
+++ table-booking-app/scripts/list.js
@@ -2,56 +2,56 @@ window.addEventListener("load",function(){
     GetBookings();
 });
   
-  function GetBookings(){
-let url = 'https://api.sheety.co/d2f6c22aad720df1ddb808edebcc390a/bookingApIdemo/sheet1';
-  fetch(url)
-  .then((response) => response.json())
-  .then(json => {
-    // Do something with the data
-    //console.log(json.sheet1);
-    var bookings = document.getElementById("booking-list");
-    var bookingIds=[];
+function GetBookings(){
+  let url = 'https://api.sheety.co/d2f6c22aad720df1ddb808edebcc390a/bookingApIdemo/sheet1';
+    fetch(url)
+    .then((response) => response.json())
+    .then(json => {
+      // Do something with the data
+      //console.log(json.sheet1);
+      var bookings = document.getElementById("booking-list");
+      var bookingIds=[];
 
-   for (var i = 0; i < json.sheet1.length; i++){
-      var gName = json.sheet1[i].name;
-      var gEmail = json.sheet1[i].email;
-      var gPax = json.sheet1[i].pax;
-      var gId = json.sheet1[i].id;
-      var gRemarks = json.sheet1[i].remarks;
-      var buttonId = "delete" + gId;
+    for (var i = 0; i < json.sheet1.length; i++){
+        var gName = json.sheet1[i].name;
+        var gEmail = json.sheet1[i].email;
+        var gPax = json.sheet1[i].pax;
+        var gId = json.sheet1[i].id;
+        var gRemarks = json.sheet1[i].remarks;
+        var buttonId = "delete" + gId;
 
-      let row = bookings.insertRow(bookings.rows.length);
-      row.insertCell(0).innerHTML = gId;  
-      row.insertCell(1).innerHTML = gName;
-      row.insertCell(2).innerHTML = gEmail;
-      row.insertCell(3).innerHTML = gPax;
-      row.insertCell(4).innerHTML = gRemarks; 
-      row.insertCell(5).innerHTML = "<button id='" + buttonId + "' class='btn btn-danger'>Delete</button></br>"; 
+        let row = bookings.insertRow(bookings.rows.length);
+        row.insertCell(0).innerHTML = gId;  
+        row.insertCell(1).innerHTML = gName;
+        row.insertCell(2).innerHTML = gEmail;
+        row.insertCell(3).innerHTML = gPax;
+        row.insertCell(4).innerHTML = gRemarks; 
+        row.insertCell(5).innerHTML = "<button id='" + buttonId + "' class='btn btn-danger'>Delete</button></br>"; 
 
-      bookingIds.push(buttonId);
-      
-    }
+        bookingIds.push(buttonId);
+        
+      }
 
     for (let j=0; j<bookingIds.length; j++){
-      let el=document.getElementById(bookingIds[j]);
-      el.addEventListener("click", function(){
-       let theId=bookingIds[j].replace("delete",""); 
-         DeleteBooking(theId);
-      });
+        let el=document.getElementById(bookingIds[j]);
+        el.addEventListener("click", function(){
+        let theId=bookingIds[j].replace("delete",""); 
+          DeleteBooking(theId);
+        });
+      }  
+      
+    });
     }
-    
-   });
-  }
- 
+  
 
-  function DeleteBooking(theId){
+function DeleteBooking(id){
     
     if(confirm("Are you sure you want to delete?")){
 
-    //let url = 'https://api.sheety.co/d2f6c22aad720df1ddb808edebcc390a/bookingApi/sheet1/'+theId;
-     let url = 'https://api.sheety.co/d2f6c22aad720df1ddb808edebcc390a/bookingApIdemo/sheet1'+theId;
+    //let url = 'https://api.sheety.co/d2f6c22aad720df1ddb808edebcc390a/bookingApi/sheet1/'+id;
+     let url = 'https://api.sheety.co/d2f6c22aad720df1ddb808edebcc390a/bookingApIdemo/sheet1'+id;
       fetch(url, {
-        method: 'DELETE',
+        method: 'DELETE'
       })
       .then((response) => {
         location.reload();
