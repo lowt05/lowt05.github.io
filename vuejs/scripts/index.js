66791695b660e14cde3b66d616456fbc116f0b13
TzeYan Low <lowt05@yahoo.com>
2020-10-30 20:52:27 +0800

    2052

--- vuejs/scripts/index.js
+++ vuejs/scripts/index.js
@@ -25,10 +25,10 @@ var app4 = new Vue({
     el: "#app-4",
     data: {
         todos:[
-            {text:"Learn HTML - Done"}.
-            {text:"Learn CSS - Done"}.
-            {text:"Learn JS - Done"}.
-            {text:"Learn Vue - In Progress"}.            
+            {text:"Learn HTML - Done"},
+            {text:"Learn CSS - Done"},
+            {text:"Learn JS - Done"},
+            {text:"Learn Vue - In Progress"}            
         ]
     }
 });
